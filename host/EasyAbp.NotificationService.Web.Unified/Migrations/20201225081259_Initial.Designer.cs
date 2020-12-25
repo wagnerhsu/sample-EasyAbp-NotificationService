1bfe2f31a42a0e49bc2e46038f2a52cd977fb8df
Wagner Xu <wagnerhsu@hotmail.com>
2020-12-25 16:42:54 +0800

    Update Abp to 4.0.2

--- host/EasyAbp.NotificationService.Web.Unified/Migrations/20201225081259_Initial.Designer.cs
+++ host/EasyAbp.NotificationService.Web.Unified/Migrations/20201225081259_Initial.Designer.cs
@@ -4,15 +4,17 @@ using EasyAbp.NotificationService.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
 namespace EasyAbp.NotificationService.Web.Unified.Migrations
 {
     [DbContext(typeof(UnifiedDbContext))]
-    partial class UnifiedDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201225081259_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
